36737da31fd5b9a3cf1a332730260be8be08f964
P Dinesh Kumar <146532356+dineshkumarpiriya@users.noreply.github.com>
2025-08-12 12:29:37 +0530

    Add files via upload

---
+++ FamilyDetails.java
@@ -0,0 +1,18 @@
+public class FamilyDetails {
+    public static void main(String[] args) {
+
+        //  Family Details
+        System.out.println("----- Family Details -----");
+        System.out.println("Father's Name : P Bhaskar Rao");
+        System.out.println("Mother's Name : P Jagamu");
+        System.out.println("Total Members : 3");
+
+        System.out.println(); // blank line
+
+        //  Student Details
+        System.out.println("----- Student Details -----");
+        System.out.println("Name   : P Dinesh Kumar");
+        System.out.println("Age    : 21");
+        System.out.println("Course : B.Tech - CSE");
+    }
+}
